6cb0487c607fbd55f1763556b4f1a55dcf40ba46
James Luty <luty0125@gmail.com>
2022-12-30 17:07:11 -0800

    Updates to navbar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -49,7 +49,6 @@ const Home = () => {
                     <h1 className="bannerHeader">Need help with taking care of your trees?</h1>
                     <h2 className="bannerSubText">With over 10 years of experience we are here to help you with your tree care needs.</h2>
                     <p className="bannerDetails">We provide a range of services such trimming, pruning, tree removal, stump grinding and more.</p>
-                    {/* <h1 className="slogan">Green Belt Tree Care. We'll go out on a limb for you.</h1> */}
                 </div>
 
                 {/* Contact section */}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import company_logo from '../imgs/company_logo.png';
+import menu_icon from '../imgs/menu_icon.png';
 
 const Navbar = () => {
     const showDropdown = () => {
@@ -11,11 +12,32 @@ const Navbar = () => {
         document.getElementById("dropdown").style.display = "none";
     }
 
+    const openMenu = () => {
+        document.getElementById("menuItems").style.display = "flex";
+    }
+
+    const closeMenu = () => {
+        document.getElementById("menuItems").style.display = "none";
+    }
+
+    const openServices = () => {
+        document.getElementById("dropdownMd").style.display = "flex";
+    }
+
+    const closeServices = () => {
+        document.getElementById("dropdownMd").style.display = "none";
+    }
+
     return (
         <div className="navbar">
-            <img className="logo" src={company_logo} alt="logo"/>
-            <h1 className="companyName">Green Belt Tree Care LLC</h1>
-            <div className="links">
+            <div className="navbarLeft">
+                <img className="logo" src={company_logo} alt="logo"/>
+                <div>
+                    <h1 className="companyName">Green Belt Tree Care LLC</h1>
+                    <h2 className="slogan">"We'll go out on a limb for you"</h2>
+                </div>
+            </div>
+            <div className="navLinks">
                 <Link to="/" className="link"><h2 className="link">Home</h2></Link>
                 <div onMouseOver={showDropdown} onMouseOut={hideDropdown}>
                     <div className="serviceNav">
@@ -34,6 +56,29 @@ const Navbar = () => {
                     </div>
                 </div>
                 <Link to="/contact" className="link"><h2 className="link">Contact Us</h2></Link>
+            </div>
+            <div onMouseOver={openMenu} onMouseOut={closeMenu} className="navLinksMd">
+                <img src={menu_icon} alt="menu_icon" className="menuIcon"/>
+                <div id="menuItems">
+                    <Link to="/" onClick={closeMenu} className="linkStyle"><h2 className="link">Home</h2></Link>
+                    <div onClick={openServices}>
+                        <div className="serviceNav">
+                            <h2 className="link">Services</h2>
+                        </div>
+                        <div id="dropdownMd">
+                            <ul className="serviceDropdown">
+                                <Link to="/services/tree-trimming" onClick={closeMenu} className="navServLink"><li className="dropdownItem">Tree Trimming</li></Link>
+                                <Link to="/services/tree-removal" onClick={closeMenu} className="navServLink"><li className="dropdownItem">Tree Removal</li></Link>
+                                <Link to="/services/ornamental-pruning" onClick={closeMenu} className="navServLink"><li className="dropdownItem">Ornamental Pruning</li></Link>
+                                <Link to="/services/crown-raising" onClick={closeMenu} className="navServLink"><li className="dropdownItem">Crown Raising</li></Link>
+                                <Link to="/services/crown-reduction"onClick={closeMenu} className="navServLink"><li className="dropdownItem">Crown Reduction</li></Link>
+                                <Link to="/services/cabling" onClick={closeMenu} className="navServLink"><li className="dropdownItem">Cabling</li></Link>
+                                <Link to="/services/stump-grinding" onClick={closeMenu} className="navServLink"><li className="dropdownItem">Stump Grinding</li></Link>
+                            </ul>
+                        </div>
+                    </div>
+                    <Link to="/contact" onClick={closeMenu} className="linkStyle"><h2 className="link">Contact Us</h2></Link>
+                </div>
             </div>
         </div>
     )
