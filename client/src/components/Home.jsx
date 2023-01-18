f3d821167800e6efdfc8a01c82c6cf51f09ad424
James Luty <luty0125@gmail.com>
2023-01-17 16:37:15 -0800

    added banner below header image

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -55,7 +55,7 @@ const Home = () => {
                     <img src={header_img} alt="tree_background" className="tree_background"/>
                 </div>
                 {/* Banner section */}
-                <div  className="bannerDiv">
+                <div className="bannerDiv">
                     <h1 className="bannerHeader">Need help with taking care of your trees?</h1>
                     <h2 className="bannerSubText">With over 10 years of experience we are here to help you with your tree care needs.</h2>
                     <p className="bannerDetails">We provide a wide range of services such as trimming, pruning, tree removal, stump grinding, and more.</p>
