b9963d81e359483423d665ff29802655dfb4588a
James Luty <luty0125@gmail.com>
2023-02-15 10:56:42 -0800

    Add stump grinding and cname

--- client/src/components/Stump.jsx
+++ client/src/components/Stump.jsx
@@ -13,7 +13,8 @@ const Stump = () => {
 
                 <div className="serviceSection">
                     <div className="serviceLeft">
-                        <h2 className="serviceSubTitle">Why should I consider stump grinding?</h2>
+                        <h2 className="serviceSubTitle">What is stump grinding?</h2>
+                        <p className="serviceDesc">A stump grinder is a machine that is designed to rolled on top of the stump and spins at a high speed to chew up the stump until the stump is no longer visible above the surface of the top soil. How deep a stump should be ground down depends on what your plans on for the area the stump is located. In most cases grinding a stump down to about 6 inches below surface level is enough. In some cases you may need to go deeper if you plan to have some sort of structure over where the stump location. After grinding down the stump there's usually a big hole where the stump used to be. In most cases this can be filled in using the wood chips created during the grinding process, but you can also have new top soil brought in to fill the hole if preferred.</p>
                     </div>
                     <div className="serviceRight">
                         <Services />
